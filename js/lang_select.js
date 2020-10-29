1d3c2ce4ab78f8d53429c3462d6cc2777f82baf4
Nick Xie <nick@khadas.com>
2020-10-29 18:02:17 +0800

    Site updated: 2020-10-29 18:02:14

--- js/lang_select.js
+++ js/lang_select.js
@@ -1,15 +1,1 @@
-(function() {
-  'use strict';
-
-  function changeLang() {
-    var lang = this.value;
-    var canonical = this.dataset.canonical;
-    if (lang === 'en') lang = '';
-    if (lang) lang += '/';
-
-    location.href = '/' + lang + canonical;
-  }
-
-  document.getElementById('lang-select').addEventListener('change', changeLang);
-  document.getElementById('mobile-lang-select').addEventListener('change', changeLang);
-}());
+!function(){"use strict";function e(){var e=this.value,t=this.dataset.canonical;"en"===e&&(e=""),e&&(e+="/"),location.href="/"+e+t}document.getElementById("lang-select").addEventListener("change",e),document.getElementById("mobile-lang-select").addEventListener("change",e)}();
