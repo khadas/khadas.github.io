9781f74f19e924e033630d22ff5bbee245aca0e5
Nick Xie <nick@khadas.com>
2021-06-25 15:14:56 +0800

    Site updated: 2021-06-25 15:14:53

---
+++ js/lang_select.js
@@ -0,0 +1,1 @@
+!function(){"use strict";function e(){var e=this.value,t=this.dataset.canonical;"en"===e&&(e=""),e&&(e+="/"),location.href="/"+e+t}document.getElementById("lang-select").addEventListener("change",e),document.getElementById("mobile-lang-select").addEventListener("change",e)}();
